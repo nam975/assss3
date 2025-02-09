bd2df851bf1fe54afa7088b73bb6ee3bc9b2fbaa
DuongDT97 <ongvanduong29@gmail.com>
2025-02-09 21:09:47 +0700

    thay đôi controler

---
+++ assss/Controllers/controleer 1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assss.Controllers
+{
+    public class controleer_1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
