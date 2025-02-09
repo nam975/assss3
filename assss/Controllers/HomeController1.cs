3021b57764a8ff6f0c27e10a4be8b96db1e84e18
DuongDT97 <ongvanduong29@gmail.com>
2025-02-09 20:59:55 +0700

    thêm home controler

---
+++ assss/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assss.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
